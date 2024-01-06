24574a9baa86b894516cf15303178a3e78aa7563
BelenG <90286485+SBelenB@users.noreply.github.com>
2024-01-06 02:13:24 -0300

    Completed the structure of the projects section and included the first project.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,12 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
-		colors: {
-			'main-purple': '#986F93',
-			'cta-purple': '#924BB4',
-			'light-pink': '#F1C1F1',
+		extend: {
+			colors: {
+				'main-purple': '#986F93',
+				'cta-purple': '#924BB4',
+				'light-pink': '#F1C1F1',
+			},
 		},
 	},
 	plugins: [],
